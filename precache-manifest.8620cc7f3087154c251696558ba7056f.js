590d0615cc0baf60a6566c2d9648945a3f04e2bb
Jacob Rockland <jakerockland@gmail.com>
2018-11-19 01:08:55 -0700

    Updates

---
+++ precache-manifest.8620cc7f3087154c251696558ba7056f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f61592412b6d1102ca1",
+    "url": "/safesexstats/static/css/main.6979f297.chunk.css"
+  },
+  {
+    "revision": "1f61592412b6d1102ca1",
+    "url": "/safesexstats/static/js/main.1f615924.chunk.js"
+  },
+  {
+    "revision": "b823138a7b040bc4e461",
+    "url": "/safesexstats/static/js/1.b823138a.chunk.js"
+  },
+  {
+    "revision": "f6e5e82d1a194fcb2742",
+    "url": "/safesexstats/static/js/runtime~main.f6e5e82d.js"
+  },
+  {
+    "revision": "8d891ef63f4d403ab8494ca333c68479",
+    "url": "/safesexstats/index.html"
+  }
+];
